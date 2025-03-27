aaa62ac26663acacffe5872cf66536336b61f768
Patrick Erber <patrick.erber@gmail.com>
2025-03-27 19:57:09 +0100

    feat(wiki): initial draft of the wiki facade

---
+++ internal/wiki/wiki.go
@@ -0,0 +1,84 @@
+package wiki
+
+import (
+	"fmt"
+
+	"github.com/perber/wiki/internal/core/tree"
+)
+
+type Wiki struct {
+	tree *tree.TreeService
+}
+
+func NewWiki(storageDir string) (*Wiki, error) {
+	treeService := tree.NewTreeService(storageDir)
+
+	if err := treeService.LoadTree(); err != nil {
+		return nil, err
+	}
+
+	return &Wiki{
+		tree: treeService,
+	}, nil
+}
+
+func (w *Wiki) CreatePage(parentID *string, title string) (*tree.Page, error) {
+	slugService := tree.NewSlugService(w.tree.GetTree())
+
+	var parent *tree.PageNode
+	if parentID == nil {
+		parent = w.tree.GetTree()
+	} else {
+		var err error
+		parent, err = w.tree.FindPageByID(w.tree.GetTree().Children, *parentID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	slug := slugService.GenerateUniqueSlug(parent, title)
+
+	if err := w.tree.CreatePage(parentID, title, slug); err != nil {
+		return nil, err
+	}
+
+	created, err := w.tree.FindPageByID(w.tree.GetTree().Children, slug)
+	if err != nil {
+		return nil, err
+	}
+	return &tree.Page{PageNode: created}, nil
+}
+
+func (w *Wiki) GetPage(id string) (*tree.Page, error) {
+	return w.tree.GetPage(id)
+}
+
+func (w *Wiki) MovePage(id, parentID string) error {
+	return w.tree.MovePage(id, parentID)
+}
+
+func (w *Wiki) DeletePage(id string, recursive bool) error {
+	return w.tree.DeletePage(id, recursive)
+}
+
+func (w *Wiki) GetTree() *tree.PageNode {
+	return w.tree.GetTree()
+}
+
+func (w *Wiki) UpdatePage(id, title, slug, content string) error {
+	return w.tree.UpdatePage(id, title, slug, content)
+}
+
+func (w *Wiki) FindByPath(route string) (*tree.Page, error) {
+	return w.tree.FindPageByRoutePath(w.tree.GetTree().Children, route)
+}
+
+func (w *Wiki) SuggestSlug(parentID string, title string) (string, error) {
+	parent, err := w.tree.FindPageByID(w.tree.GetTree().Children, parentID)
+	if err != nil {
+		return "", fmt.Errorf("parent not found: %w", err)
+	}
+
+	slugService := tree.NewSlugService(w.tree.GetTree())
+	return slugService.GenerateUniqueSlug(parent, title), nil
+}
