64d86da40efa5d419747f85a6f0d019c4fc15566
Patrick Erber <patrick.erber@gmail.com>
2025-04-13 21:15:29 +0200

    feat: fix add page dialog

--- ui/leafwiki-ui/src/stores/addPageDialogStore.ts
+++ ui/leafwiki-ui/src/stores/addPageDialogStore.ts
@@ -8,10 +8,12 @@ import { useTreeStore } from './tree';
 
 type AddPageDialogStore = {
     open: boolean
-    parentId: string
+    parentId: string 
     title: string
     slug: string
     loading: boolean
+    success: boolean
+    parentPath: string
     errors: Record<string, string> | null;
     getSlug: (parentId: string, title: string) => Promise<void>
     openDialog: (parentId: string) => void
@@ -19,47 +21,48 @@ type AddPageDialogStore = {
     setTitle: (title: string) => void
     setFieldErrors: (errors: Record<string, string>) => void;
     setSlug: (slug: string) => void
-    resetState: () => void
     createPage: () => Promise<void>
 }
 
 export const useAddPageDialogStore = create<AddPageDialogStore>((set, get) => ({
     open: false,
-    parentId: '',
+    parentId: "",
     title: '',
     slug: '',
+    parentPath: '',
     loading: false,
+    success: false,
     errors: null,
     setFieldErrors: (errors) => set({ errors }),
-    openDialog: (parentId) => set({ parentId: parentId, title: "", slug: "", open: true, errors: null }),
-    closeDialog: () => set({ parentId: "", title: "", slug: "", open: false, errors: null }),
+    openDialog: (parentId) => {
+        const pagePath = useTreeStore.getState().getPathById(parentId) || ''
+        set({ parentId: parentId, parentPath: pagePath, title: "", slug: "", open: true, errors: null, loading: false, success: false })
+    },
+    closeDialog: () => set({ parentId: "", title: "", parentPath:"", slug: "", open: false, errors: null, loading: false, success: false }),
     setTitle: (title) => set({ title, errors: null }),
     setSlug: (slug) => set({ slug, errors: null }),
     getSlug: async (parentId, title) => {
         try {
             const slug = await suggestSlug(parentId, title)
-            set({ slug, errors: null })
+            set({ slug, errors: null, })
         } catch (error) {
             console.warn('Error generating slug:', error)
             set({ slug: '', errors: { slug: "couldn't generate slug" } })
             toast.error('Error generating slug')
         }
     },
-    resetState: () => set({ title: '', slug: '', parentId: "", open: false, errors: null, loading: false, }),
     createPage: async () => {
         try {
             set({ loading: true, errors: null })
             await createPage({ title: get().title, slug: get().slug, parentId: get().parentId })
-            set({ loading: false })
+            set({ loading: false, success: true })
             toast.success('Page created')
             // Reload the tree or navigate to the new page
             useTreeStore.getState().reloadTree()
-            set({ open: false, title: '', slug: '', parentId: "", errors: null })
         } catch (error) {
             console.warn('Error creating page:', error)
             handleFieldErrors(error, get().setFieldErrors, 'Error creating page');
-        } finally {
-            set({ loading: false })
+            set({ loading: false, success: false })
         }
 
     },
