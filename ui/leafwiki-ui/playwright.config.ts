ac5090ec9f3397431ac2abacd097144a356c9196
Patrick Erber <patrick.erber@gmail.com>
2025-05-13 18:02:41 +0200

    fix: change baseUrl in playwright test

--- ui/leafwiki-ui/playwright.config.ts
+++ ui/leafwiki-ui/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 10000,
   use: {
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:4173',
     headless: false,
     screenshot: 'only-on-failure',
   },
