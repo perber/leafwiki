771a703c6543e850edc1ec715f141ba6c9c488ab
Patrick Erber <patrick.erber@gmail.com>
2025-05-30 17:21:33 +0200

    feat: add tooltips to markdown toolbar

--- ui/leafwiki-ui/src/components/editor/MarkdownToolbar.tsx
+++ ui/leafwiki-ui/src/components/editor/MarkdownToolbar.tsx
@@ -3,6 +3,7 @@ import { AssetManager } from '@/features/page/AssetManager'
 import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
 import { Bold, Code, Image, Italic, Link, Strikethrough, Table } from 'lucide-react'
 import { useState } from 'react'
+import { TooltipWrapper } from '../TooltipWrapper'
 import { Dialog, DialogContent, DialogHeader } from '../ui/dialog'
 import { MarkdownEditorRef } from './MarkdownEditor'
 
@@ -28,58 +29,78 @@ export default function MarkdownToolbar({ editorRef, pageId }: Props) {
   return (
     <>
       <div className="sticky top-0 z-10 flex gap-1.5 border-b border-zinc-700 bg-zinc-900 p-2 shadow-sm">
-        <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('**')} className={toolbarButtonStyle}>
-          <Bold className="w-4 h-4" />
-        </Button>
-        <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('_')} className={toolbarButtonStyle}>
-          <Italic className="w-4 h-4" />
-        </Button>
-        <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('~~')} className={toolbarButtonStyle}>
-          <Strikethrough className="w-4 h-4" />
-        </Button>
-        <Button variant="ghost" size="icon" className={toolbarButtonStyle} onClick={() => editorRef.current?.insertWrappedText('[', '](https://example.com)')}>
-          <Link className="w-4 h-4" />
-        </Button>
+        <TooltipWrapper label="Bold" side="top" align="center">
+          <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('**')} className={toolbarButtonStyle}>
+            <Bold className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Italic" side="top" align="center">
+          <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('_')} className={toolbarButtonStyle}>
+            <Italic className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Strike through" side="top" align="center">
+          <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertWrappedText('~~')} className={toolbarButtonStyle}>
+            <Strikethrough className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Link" side="top" align="center">
+          <Button variant="ghost" size="icon" className={toolbarButtonStyle} onClick={() => editorRef.current?.insertWrappedText('[', '](https://example.com)')}>
+            <Link className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
         <div className="mx-1 h-5 w-px bg-white/30 self-center" />
-        <Button
-          variant="ghost"
-          size="icon"
-          className={toolbarButtonStyle}
-          onClick={() => insertHeading(1)}
-        >
-          H1
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className={toolbarButtonStyle}
-          onClick={() => insertHeading(2)}
-        >
-          H2
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className={toolbarButtonStyle}
-          onClick={() => insertHeading(3)}
-        >
-          H3
-        </Button>
+        <TooltipWrapper label="Headline - H1" side="top" align="center">
+          <Button
+            variant="ghost"
+            size="icon"
+            className={toolbarButtonStyle}
+            onClick={() => insertHeading(1)}
+          >
+            H1
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Headline - H2" side="top" align="center">
+          <Button
+            variant="ghost"
+            size="icon"
+            className={toolbarButtonStyle}
+            onClick={() => insertHeading(2)}
+          >
+            H2
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Headline - H3" side="top" align="center">
+          <Button
+            variant="ghost"
+            size="icon"
+            className={toolbarButtonStyle}
+            onClick={() => insertHeading(3)}
+          >
+            H3
+          </Button>
+        </TooltipWrapper>
         <div className="mx-1 h-5 w-px bg-white/30 self-center" />
-        <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertAtCursor(tableMarkdown)} className={toolbarButtonStyle}>
-          <Table className="w-4 h-4" />
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className={toolbarButtonStyle}
-          onClick={() => editorRef.current?.insertWrappedText('```\n', '\n```')}
-        >
-          <Code className="w-4 h-4" />
-        </Button>
-        <Button variant="ghost" size="icon" onClick={() => setAssetModalOpen(true)} className={toolbarButtonStyle}>
-          <Image className="w-4 h-4" />
-        </Button>
+        <TooltipWrapper label="Table" side="top" align="center">
+          <Button variant="ghost" size="icon" onClick={() => editorRef.current?.insertAtCursor(tableMarkdown)} className={toolbarButtonStyle}>
+            <Table className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Codeblock" side="top" align="center">
+          <Button
+            variant="ghost"
+            size="icon"
+            className={toolbarButtonStyle}
+            onClick={() => editorRef.current?.insertWrappedText('```\n', '\n```')}
+          >
+            <Code className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
+        <TooltipWrapper label="Add Image or File" side="top" align="center">
+          <Button variant="ghost" size="icon" onClick={() => setAssetModalOpen(true)} className={toolbarButtonStyle}>
+            <Image className="w-4 h-4" />
+          </Button>
+        </TooltipWrapper>
       </div >
 
       <Dialog open={assetModalOpen} onOpenChange={setAssetModalOpen}>
