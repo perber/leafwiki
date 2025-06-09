cd55dc9c99bb51def3dc943b32105533b20a024e
Patrick Erber <patrick.erber@gmail.com>
2025-06-09 20:55:28 +0200

    test(search): add search test

--- ui/leafwiki-ui/tests/auth.spec.ts
+++ ui/leafwiki-ui/tests/auth.spec.ts
@@ -29,12 +29,24 @@ test('failed login', async ({ page }) => {
     await expect(page.getByRole('button', { name: 'A' })).toBeVisible();
   });
 
-  test('create page', async ({ page }) => {
-    await login(page);
-    await page.getByRole('button').first().click();
-    await page.getByRole('textbox', { name: 'Page title' }).click();
-    await page.getByRole('textbox', { name: 'Page title' }).fill('Eine neue Seite');
-    await page.getByRole('button', { name: 'Create' }).click();
-    await expect(page.getByText('eine-neue-seite')).toBeVisible();
-  });
 
+  test('create a page', async ({ page }) => {
+    await login(page);
+    await page.locator('.btn-treeview').first().click();
+    await page.getByRole('textbox', { name: 'Page title' }).click();
+    await page.getByRole('textbox', { name: 'Page title' }).fill('Neue Seite anlegen');
+    await page.getByRole('button', { name: 'Create' }).click();
+    // CodeMirror Ediotr
+    await page.locator('.cm-content').click() 
+    await page.keyboard.type('# Hello World\n\nThis is LeafWiki.\n\n Und wir legen eine neue Seite an')  // Tippen
+    await page.locator('.sticky > div > .inline-flex').first().click();
+    await page.getByRole('banner').getByRole('button').nth(2).click();
+    await page.locator('div:nth-child(12) > .inline-flex').click();
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByRole('banner').getByRole('button').nth(1).click();
+    await page.getByRole('button', { name: 'Search' }).click();
+    await page.getByRole('textbox', { name: 'Search...' }).click();
+    await page.getByRole('textbox', { name: 'Search...' }).fill('Neue');
+    await page.getByRole('textbox', { name: 'Search...' }).press('Enter');
+    await expect(page.getByRole('link', { name: 'Neue Seite anlegen Neue Seite anlegen Hello World This is LeafWiki. Und wir legen eine neue Seite an**** / neue-seite-anlegen', exact: true })).toBeVisible();
+  });
