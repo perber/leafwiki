15a3ef35580c1c6138e717325edbfca8ca72769f
Patrick Erber <patrick.erber@gmail.com>
2025-06-17 23:28:16 +0200

    fix: title is taking to much space in the titlebar
    adjusting css to truncate title

--- ui/leafwiki-ui/src/components/EditorTitleBar.tsx
+++ ui/leafwiki-ui/src/components/EditorTitleBar.tsx
@@ -1,4 +1,6 @@
+import { useIsMobile } from '@/lib/useIsMobile'
 import { Pencil } from 'lucide-react'
+import { TooltipWrapper } from './TooltipWrapper'
 
 type Props = {
   title: string
@@ -8,23 +10,34 @@ type Props = {
 }
 
 export function EditorTitleBar({ title, slug, onEditClicked, isDirty }: Props) {
+  const isMobile = useIsMobile()
+
   return (
     <div className="flex flex-1 flex-col items-center">
       <button
         onClick={() => onEditClicked()}
         className="group relative flex items-center gap-1 text-base font-semibold text-gray-800 hover:underline"
       >
-        {title && <span>{title}</span>}
-        <Pencil
-          size={16}
-          className="absolute -right-6 top-1/2 -translate-y-1/2 text-gray-400 transition-transform duration-200 ease-in-out group-hover:text-gray-600"
-        />
-        {isDirty && (
-          <span className="ml-2 text-xs text-yellow-600">(Changes)</span>
-        )}
-      </button>
+        <TooltipWrapper label={title} side="top" align="start">
+          {title && (
+            <span className="max-w-[15vw] truncate sm:max-w-[40vw]">
+              {title}
+            </span>
+          )}
+          <Pencil
+            size={16}
+            className="absolute -right-6 top-1/2 -translate-y-1/2 text-gray-400 transition-transform duration-200 ease-in-out group-hover:text-gray-600"
+          />
+          {isDirty && !isMobile && (
+            <span className="ml-2 text-xs text-yellow-600">(Changes)</span>
+          )}
 
-      <span className="mt-1 rounded bg-gray-200 px-2 py-0.5 font-mono text-xs text-gray-700">
+          {isDirty && isMobile && (
+            <span className="ml-2 text-xs text-yellow-600">*</span>
+          )}
+        </TooltipWrapper>
+      </button>
+      <span className="mt-1 max-w-[15vw] truncate rounded bg-gray-200 px-2 py-0.5 font-mono text-xs text-gray-700 sm:max-w-[40vw]">
         {slug}
       </span>
     </div>
