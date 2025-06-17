1decf1473c207094e9277a308cfd0c7213886806
Patrick Erber <patrick.erber@gmail.com>
2025-06-17 23:08:33 +0200

    fix: the editor isn't taking the whole place

--- ui/leafwiki-ui/src/features/editor/MarkdownEditor.tsx
+++ ui/leafwiki-ui/src/features/editor/MarkdownEditor.tsx
@@ -294,11 +294,11 @@ const MarkdownEditor = (
       {/* Desktop */}
       <div className="flex h-full w-full flex-col max-md:hidden">
         {renderToolbar()}
-        <div className="flex w-full overflow-hidden">
-          <div className="w-1/2 max-w-none overflow-auto">
+        <div className="flex w-full overflow-hidden flex-1">
+          <div className="w-1/2 flex flex-1 max-w-none overflow-auto">
             {renderEditor(false)}
           </div>
-          <div className="w-1/2 max-w-none">{renderPreview()}</div>
+          <div className="w-1/2 max-w-none flex-1">{renderPreview()}</div>
         </div>
       </div>
     </div>
