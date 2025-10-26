20afc707903d173ca134d15d1d3aec93978f7c19
Patrick Erber <patrick.erber@gmail.com>
2025-10-26 14:58:04 +0100

    chore: format code

--- ui/leafwiki-ui/src/features/page/CopyPageDialog.tsx
+++ ui/leafwiki-ui/src/features/page/CopyPageDialog.tsx
@@ -183,9 +183,7 @@ export function CopyPageDialog({ sourcePage }: { sourcePage: Page }) {
               disabled={isCopyButtonDisabled}
             >
               {loading ? 'Copying...' : 'Copy'}{' '}
-              {loading && (
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              )}
+              {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
             </Button>
           </FormActions>
         </div>
