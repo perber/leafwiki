dcfa3246f9f96c5249f5c3d7b10db48af8568e02
Patrick Erber <patrick.erber@gmail.com>
2025-10-30 22:46:55 +0100

    fix: remove unused hashCode function

--- ui/leafwiki-ui/src/lib/utils.ts
+++ ui/leafwiki-ui/src/lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function hashString(str: string) {
-  let h = 0
-  for (let i = 0; i < str.length; i++) {
-    h = (h * 31 + str.charCodeAt(i)) | 0
-  }
-  return (h >>> 0).toString(36)
-}
