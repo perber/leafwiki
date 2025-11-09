5850c50d53163d038499579ae391cafd53d56bb1
Patrick Erber <patrick.erber@gmail.com>
2025-11-09 12:30:23 +0100

    feat: use env variable to overwrite headless configuration

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
 const baseURL = process.env.E2E_BASE_URL || 'http://localhost:8080';
+const headless = process.env.E2E_HEADLESS !== 'false';
 
 export default defineConfig({
   testDir: './tests',
   timeout: 10000,
   use: {
     baseURL,
-    headless: true,
+    headless,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
