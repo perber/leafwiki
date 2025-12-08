63dc98832b6600972d000803f76ee12195a1e5cc
Patrick Erber <patrick.erber@gmail.com>
2025-12-08 18:57:05 +0100

    feat: integrate outgoing links into frontend

--- internal/http/router.go
+++ internal/http/router.go
@@ -78,6 +78,7 @@ func NewRouter(wikiInstance *wiki.Wiki, publicAccess bool, injectCodeInHeader st
 			nonAuthApiGroup.GET("/pages/lookup", api.LookupPagePathHandler(wikiInstance))
 			nonAuthApiGroup.GET("/pages/:id", api.GetPageHandler(wikiInstance))
 			nonAuthApiGroup.GET("/pages/:id/backlinks", api.GetBacklinksHandler(wikiInstance))
+			nonAuthApiGroup.GET("/pages/:id/outgoing-links", api.GetOutgoingLinksHandler(wikiInstance))
 
 			// Search
 			nonAuthApiGroup.GET("/search/status", api.SearchStatusHandler(wikiInstance))
@@ -96,6 +97,7 @@ func NewRouter(wikiInstance *wiki.Wiki, publicAccess bool, injectCodeInHeader st
 			requiresAuthGroup.GET("/pages/lookup", api.LookupPagePathHandler(wikiInstance))
 			requiresAuthGroup.GET("/pages/by-path", api.GetPageByPathHandler(wikiInstance))
 			requiresAuthGroup.GET("/pages/:id/backlinks", api.GetBacklinksHandler(wikiInstance))
+			requiresAuthGroup.GET("/pages/:id/outgoing-links", api.GetOutgoingLinksHandler(wikiInstance))
 
 			// Search
 			requiresAuthGroup.GET("/search/status", api.SearchStatusHandler(wikiInstance))
