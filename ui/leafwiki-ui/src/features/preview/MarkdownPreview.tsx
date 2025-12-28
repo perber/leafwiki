7c055f43854e4b577fb64b3cee589203042da851
perber <patrick.erber@gmail.com>
2025-12-28 19:22:36 +0100

    feat: allow css styles in markdown (#504)

--- ui/leafwiki-ui/src/features/preview/MarkdownPreview.tsx
+++ ui/leafwiki-ui/src/features/preview/MarkdownPreview.tsx
@@ -11,12 +11,13 @@ import { MarkdownImage } from './MarkdownImage'
 import { MarkdownLink } from './MarkdownLink'
 import MermaidBlock from './MermaidBlock'
 import { rehypeLineNumber } from './rehypeLineNumber'
+import { rehypeWhitelistStyles } from './rehypeWhitelistStyles'
 
 const schema = {
   ...defaultSchema,
   attributes: {
     ...defaultSchema.attributes,
-    '*': [...(defaultSchema.attributes?.['*'] || []), 'data-line'],
+    '*': [...(defaultSchema.attributes?.['*'] || []), 'data-line', 'style'],
   },
 }
 
@@ -142,6 +143,7 @@ export default function MarkdownPreview({ content, path }: Props) {
         rehypePlugins={[
           rehypeRaw,
           rehypeLineNumber,
+          rehypeWhitelistStyles,
           [rehypeSanitize, schema],
           rehypeHighlight,
         ]}
